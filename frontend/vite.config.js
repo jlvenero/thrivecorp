e516510a865380c185d4de68ea085915bf3ab378
Lucas Martins venero <lucasmvenero@gmail.com>
2025-11-06 13:51:26 -0300

    config-tests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Permite usar 'describe', 'it', 'expect' sem importar
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js', // Arquivo de setup (vamos criar a seguir)
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'], // Importante: 'lcov' para o SonarCloud
+      reportsDirectory: './coverage' // Onde salvar os relatórios
+    }
+  }
 })
